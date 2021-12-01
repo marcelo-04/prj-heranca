748c9158b466c085ff134bc113237d40f6acd2c4
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2021-12-01 19:57:33 -0300

    Criando as Classes Diretor, Secretario e teste na main

---
+++ src/br/com/modulo10/Secretario.java
@@ -0,0 +1,36 @@
+package br.com.modulo10;
+
+public class Secretario extends Pessoa {
+	
+	private String registro;
+	private String nivelCargo;
+	private String tempoExperiencia;
+	
+	public String getRegistro() {
+		return registro;
+	}
+	public void setRegistro(String registro) {
+		this.registro = registro;
+	}
+	public String getNivelCargo() {
+		return nivelCargo;
+	}
+	public void setNivelCargo(String nivelCargo) {
+		this.nivelCargo = nivelCargo;
+	}
+	public String getTempoExperiencia() {
+		return tempoExperiencia;
+	}
+	public void setTempoExperiencia(String tempoExperiencia) {
+		this.tempoExperiencia = tempoExperiencia;
+	}
+	
+	@Override
+	public String toString() {
+		return "Secretario [registro=" + registro + ", nivelCargo=" + nivelCargo + ", tempoExperiencia="
+				+ tempoExperiencia + ", nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento
+				+ ", numeroRg=" + numeroRg + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai="
+				+ nomePai + "]";
+	}
+	
+}
--- src/br/com/teste/modulo10/ClasseTeste.java
+++ src/br/com/teste/modulo10/ClasseTeste.java
@@ -1,6 +1,8 @@
 package br.com.teste.modulo10;
 
 import br.com.modulo10.Aluno;
+import br.com.modulo10.Diretor;
+import br.com.modulo10.Secretario;
 
 public class ClasseTeste {
 
@@ -11,7 +13,20 @@ public class ClasseTeste {
 		aluno.setIdade(52);
 		aluno.setNomeEscola("Jdev - Treinamento");
 		
+		Diretor diretor = new Diretor();
+		diretor.setNome("José Antonio");
+		diretor.setIdade(55);
+		diretor.setTempoDirecao(3);
+		
+		Secretario secretario = new Secretario();
+		secretario.setNome("Marcos Silva");
+		secretario.setIdade(36);
+		secretario.setNivelCargo("Administrador");
+		
+		
 		System.out.println(aluno);
+		System.out.println(diretor);
+		System.out.println(secretario);
 
 	}
 
