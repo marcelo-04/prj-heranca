92516e865213f599a68156cb600ea054510185f2
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2021-12-14 19:18:01 -0300

    Criado a interface StatusAluno

---
+++ src/br/com/constantes/modulo10/StatusAluno.java
@@ -0,0 +1,8 @@
+package br.com.constantes.modulo10;
+
+public interface StatusAluno {
+
+    public static String APROVADO = "Aprovado";
+    public static String RECUPERACAO = "Recuperacao";
+    public static  String REPROVADO = "Reprovado";
+}
